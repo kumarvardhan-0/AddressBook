9cfda8b0577778c204e7b1fc067e6a2e5a62ff88
Kumarvardhan <gajulakumarvardhan230@gmail.com>
2025-05-20 16:16:23 +0530

    UC4 : Added Service layer

---
+++ src/main/java/com/example/demo/repository/AddressBookRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+
+
+import com.example.demo.model.AddressBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressBookRepository extends JpaRepository<AddressBook, Integer> {
+}
